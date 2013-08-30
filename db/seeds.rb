4eab44b25afbd2e2be35bd9c484751507c11c883
Dimitry Snezhkov <dsnehkov@gmail.com>
2013-08-30 12:15:29 -0500

    seed included

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Priority.create(prioritynumber: 1, priorityname: 'High')
+Priority.create(prioritynumber: 2, priorityname: 'Medium')
+Priority.create(prioritynumber: 3, priorityname: 'Low')
+Priority.create(prioritynumber: 4, priorityname: 'Info')
+Priority.create(prioritynumber: 5, priorityname: 'Best Practice')
+
+State.create(statename: 'L1')
+State.create(statename: 'L2')
+State.create(statename: 'L3')
