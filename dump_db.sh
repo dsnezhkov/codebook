12f8cc383c36138a57bc72f58b67686a5b82b2a3
Dimitry Snezhkov <dsnehkov@gmail.com>
2013-08-30 12:44:55 -0500

    README improvements

--- dump_db.sh
+++ dump_db.sh
@@ -1,11 +1,13 @@
 # this is useful if we need to "attach" data to existing db
-# direct data dump into YAML format
+# direct data dump into YAML format ( gem 'yaml_db' )
 rake db:data:dump
 
 # load back into db
 RAILS_ENV=development rake db:data:load 
 
-# load schema from schema.rb
-# rake db:schema:load
+# load schema nd data back
+rake db:schema:dump         # Create a db/schema.rb file that can be portably used against any DB supported by AR
+rake db:schema:load         # Load a schema.rb file into the database
+rake db:load                # Load schema and data from db/schema.rb and db/data.yml
 
 
