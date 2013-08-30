0f744114ee023cedbe6e38252eb461e30d484b2d
Dimitry Snezhkov <dsnehkov@gmail.com>
2013-08-30 08:47:49 -0500

    GitIgnore migration 3

--- db/migrate/20130815025652_add_state_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddStateToComment < ActiveRecord::Migration
-  def change
-    add_column :comments, :state, :string
-  end
-end
--- db/migrate/20130815032207_change_columns_on_comment.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnsOnComment < ActiveRecord::Migration
-  def change_table
-	rename_column :comment, :body, :description
-	rename_column :comment, :commenter, :title
-	change_column :comment, :description, :text 
-  end
-end
